841af8701fd1463c48e36eed9120320c8d88a8d3
chmod <talr@TALR1-TP.haifa.ibm.com>
2016-11-29 11:55:25 +0200

    comment

--- temperture-converter/src/test/java/com/packtpub/mastering/selenium/RunCukesTest.java
+++ temperture-converter/src/test/java/com/packtpub/mastering/selenium/RunCukesTest.java
@@ -11,9 +11,10 @@ import cucumber.api.junit.Cucumber;
  * Created by talr on 15/11/2016.
  */
 @RunWith(Cucumber.class)
+
+// was good for older verson of cucumber (1.0.14)
 //@Cucumber.Options(format = {"pretty", "html:target/cucumber-htmlreport","json-pretty:target/cucumber-report.json","junit:target/cucumber-results.xml"})
-//@CucumberOptions(format = {"pretty", "html:target/cucumber-htmlreport","json-pretty:target/cucumber-report.json","junit:target/cucumber-results.xml"})
-//(working)// @CucumberOptions(format = "pretty","html:target/cucumber-htmlreport","json-pretty:target/cucumber-report.json","junit:target/cucumber-results.xml")
+
 @CucumberOptions( plugin = {"pretty", "html:target/cucumber-html-report", "json:target/cucumber-json-report.json" })
 
 
