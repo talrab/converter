a9980278427c18c37bf343cb2b689e36b731a961
chmod <talr@TALR1-TP.haifa.ibm.com>
2016-11-30 09:41:00 +0200

    comment

--- temperture-converter/src/test/java/com/packtpub/mastering/selenium/RunCukesTest.java
+++ temperture-converter/src/test/java/com/packtpub/mastering/selenium/RunCukesTest.java
@@ -18,5 +18,6 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions( plugin = {"pretty", "html:target/cucumber-html-report", "json:target/cucumber-json-report.json" })
 
 
+
 public class RunCukesTest {
 }
